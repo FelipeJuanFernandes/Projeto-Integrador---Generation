9567ea9a8fd4d30b72e05348c0bccd7c14276864
Gisele R. Mascarenhas <gisele.mascarenhass@outlook.com>
2021-10-16 13:34:36 -0300

    Atualização da Controller de Usuário e Usuário Service

--- backend/src/main/java/br/org/generation/bluegeneration/controller/UsuarioController.java
+++ backend/src/main/java/br/org/generation/bluegeneration/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,6 +31,15 @@ public class UsuarioController {
 	public ResponseEntity <List<Usuario>> getAll(){
 		
 		return ResponseEntity.ok(usuarioService.listarUsuarios());
+		
+	}
+	
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> getById(@PathVariable long id) {
+		return usuarioService.buscarUsuarioId(id)
+			.map(resp -> ResponseEntity.ok(resp))
+			.orElse(ResponseEntity.notFound().build());
 		
 	}
 	
